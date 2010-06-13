2bdb8612d84d82fbfba0405dcf72d658e14805e3
Michaël Witrant <mike@lepton.fr>
2010-06-13 20:00:00 +0200

    parse language names

--- lib/ttfunk/table/cff.rb
+++ lib/ttfunk/table/cff.rb
@@ -17,9 +17,34 @@ module TTFunk
           # Reference : http://www.adobe.com/devnet/font/pdfs/5176.CFF.pdf
 
           major, minor, hdr_size, off_size = read(4, 'c*')
-          p [major, minor]
-          p [hdr_size, off_size]
+          
+          raise "Unknown CFF version: #{major}.#{minor}" if [major, minor] != [1, 0]
+          raise "Invalid header" if hdr_size != 4
+          
+          offsets = read_index_offsets
+          language_names = parse_index_offsets(offsets)
+          p language_names
+          
           raise NotImplementedError, "To be continued..."
+        end
+        
+        def read_index_offsets
+          count, off_size = read(3, 'nc')
+          raise "Unsupported offSize" if off_size != 1
+          offsets = read(count + 1, "c*")
+          offsets.map { |offset| io.pos - 1 + offset }
+        end
+        
+        def parse_index_offsets(offsets)
+          data = []
+          offsets.each_with_index do |offset, i|
+            break if i == offsets.size - 1
+            parse_from(offset) do
+              size = offsets[i + 1] - offset
+              data << read(size, 'a*').first
+            end
+          end
+          data
         end
     end
   end
